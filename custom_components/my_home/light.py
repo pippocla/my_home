2b6fff22283667c1c033713bdd3f832fdd021526
pippocla <37950200+pippocla@users.noreply.github.com>
2019-10-13 19:55:08 +0200

    Add files via upload

--- custom_components/my_home/light.py
+++ custom_components/my_home/light.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Support for My Home Platform TEST light71
+Support for My Home Platform 
 
 """
 import logging
@@ -37,60 +37,39 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the My Home Platform TEST"""
-
-
-    #gate_data = hass.data[DOMAIN]
     gate = hass.data[DOMAIN]
-    print('gate',gate)
-    #gate=OpenWebNet(gate_data[0],gate_data[1],gate_data[2])
     add_devices(MyHomeLight(light,gate) for light in config[CONF_DEVICES])
 
 class MyHomeLight(Light):
     """ Rappresentazione di una luce My Home """
-
-
     def __init__ (self,light,gate):
         """Inizializzo MyHome light"""
-
         self._gate = gate
         self._name = light[CONF_NAME]
         self._indirizzo = light['indirizzo']
         self._state = False
         self._first = {}
 
-
-
     @property
     def name(self):
         """Return the display name of this light """
-
-
         return self._name
-
-
 
     @property
     def is_on(self):
         """Return true if the light is on """
-
         return self._state
 
     def turn_on(self, **kwargs):
         """Instruct the light to turn on"""
-
         self._gate.cmd_open("1","1",self._indirizzo)
-
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off"""
-
         self._gate.cmd_open("1","0",self._indirizzo)
 
-
     def update(self):
-
         if not self._first.get(self._indirizzo) :
-
             self._first[self._indirizzo]='1'
             print('self_first update',self._first)
             self._gate.cmd_open("1",'',self._indirizzo)
--- custom_components/my_home/sensor.py
+++ custom_components/my_home/sensor.py
@@ -53,20 +53,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the My Home Platform TEST"""
-
-
-
     gate = hass.data[DOMAIN]
-
     add_devices(MyHomeTempSensor(sensor,gate) for sensor in config[CONF_DEVICES])
 
 class MyHomeTempSensor(Entity):
     """ Rappresentazione di un Sensore di Temperatura My Home """
-    #import OpenWebNet
-
     def __init__ (self,sensor_type,gate):
-        """Inizializzo MyHome light"""
-
+        """Inizializzo MyHome """
         self._gate = gate
         self.type = sensor_type['type']
         self._name = sensor_type['Name']
@@ -79,26 +72,22 @@ class MyHomeTempSensor(Entity):
 
     @property
     def name(self):
-
         """Return the display name of this light """
         return self._name
 
     @property
     def device_clas(self):
-
-        """Retunr the class of this sensor."""
+        """Return the class of this sensor."""
         return SENSOR_TYPES.get(self.type)[3]
 
     @property
     def icon(self):
-
         """Icon to use in the frontend, if any."""
         return SENSOR_TYPES.get(self.type)[2]
 
 
     @property
     def state(self):
-
         """Return state of the sensor """
         return self._state
 
@@ -108,9 +97,6 @@ class MyHomeTempSensor(Entity):
         return self._unit_of_measurement
 
     def update(self):
-
-        print('update sensor type',self.type)
-        print('update sensor name',self._name)
         if self.type == 'SetTemperature':
             self._gate.cmd_open("4",'14',self._indirizzo)
             self._state = self._gate.setTemperature_status(self._indirizzo)
@@ -121,22 +107,22 @@ class MyHomeTempSensor(Entity):
             self._gate.cmd_open('4','19',self._indirizzo)
             self._state = self._gate.valveState_status(self._indirizzo)
         elif self.type == 'Gateway_Time':
-            print('Gateway Time request from hass')
+
             self._gate.cmd_open('13','0','')
             self._state = self._gate.gateway_Status('0')
         elif self.type == 'Gateway_IP':
-            print('Gateway IP request from hass')
+
             self._gate.cmd_open('13','10','')
             self._state = self._gate.gateway_Status('10')
         elif self.type == 'Gateway_Model':
-            print('Gateway Model request from hass')
+
             self._gate.cmd_open('13','15','')
             self._state = self._gate.gateway_Status('15')
         elif self.type == 'Gateway_Firmware':
-            print('Gateway Firmware request from hass')
+
             self._gate.cmd_open('13','16','')
             self._state = self._gate.gateway_Status('16')
         elif self.type == 'Gateway_Uptime':
-            print('Gateway Uptime request from hass')
+
             self._gate.cmd_open('13','19','')
             self._state = self._gate.gateway_Status('19')
